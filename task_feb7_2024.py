8f4c225cd0f3ca5a39e0986994f3a219cb48615c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-08 21:17:30 -0600

    add program

--- task_feb7_2024.py
+++ task_feb7_2024.py
@@ -9,3 +9,17 @@ radius = float(input("Enter the radius of a circle: "))
 
 area_of_circle = calculate_circle_area(radius)
 print(f"The area of the circle with radius {radius} is: {area_of_circle}")
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num1 < num2:
+    print(f"{num1} is less than {num2}")
+else:
+    print(f"{num1} is equal to {num2}")
