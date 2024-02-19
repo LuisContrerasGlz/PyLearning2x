057330551aa21b2033ecdfcbdf7cad763f5d2a39
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-19 15:31:48 -0600

    progress

--- task_feb_14_2024.py
+++ task_feb_14_2024.py
@@ -13,3 +13,20 @@ original_string = "Hello, World!"
 reversed_string2 = ''.join(reversed(original_string))
 print(reversed_string2)
 
+# Using for Loop
+
+or_str = "Using a for loop now"
+rev_or_srt = ""
+for i in or_str:
+    rev_or_srt =  i + rev_or_srt
+print(rev_or_srt)
+
+# Making it a function
+
+def rev_str_for(or_str2):
+    rev_or_srt2 = ""
+    for i in or_str2:
+        rev_or_srt2 =  i + rev_or_srt2
+    return rev_or_srt2
+
+print(rev_str_for("Using a for loop now"))
