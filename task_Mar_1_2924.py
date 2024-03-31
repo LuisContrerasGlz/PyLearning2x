1248dd6cb4dfbf57c4761ab3fd68f7c2dd1b4684
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-03-30 18:26:50 -0600

    add progress

---
+++ task_Mar_1_2924.py
@@ -0,0 +1,13 @@
+# Creating custom exception with python
+
+class MyCustomException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+balance = 100
+withdraw_amount = int(input("Enter the amount you can withdraw: "))
+
+if withdraw_amount > balance:
+    raise MyCustomException("Bal is low!")
+else:
+    print("Total after withdraw ", balance-withdraw_amount)
