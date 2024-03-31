041c856737ee7fe3eac075ed6c1a2f04938029aa
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-03-31 01:17:21 -0600

    add progress

--- task_Mar_1_2924_file_handling.py
+++ task_Mar_1_2924_file_handling.py
@@ -33,3 +33,19 @@ content = file.read()
 print(content)
 file.close
 
+# Same but using with keyword 
+
+try:
+    with open ("TestData.txt", "r") as file2:
+        content2 = file2.read()
+        print(content2)
+except FileNotFoundError as fnfr:
+    print(fnfr)
+
+"""
+when using the with statement in Python for file handling, you don't need to explicitly close the file. 
+The with statement ensures that the file is automatically closed when the block of code inside it exits, even if an exception occurs within the block.
+
+It's considered a best practice to use the with statement for file handling in Python because it simplifies code and helps prevent common errors related to file management, such as forgetting to close the file.
+
+"""
